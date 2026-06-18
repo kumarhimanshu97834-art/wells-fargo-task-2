64a392db0a8d2eb177ac8f6f7f2f52aac7c05c30
kumarhimanshu97834-art <kumarhimanshu97834@gmail.com>
2026-06-18 22:23:46 +0530

    Update Client.java

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,1 +1,63 @@
+package com.wellsfargo.counselor.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue()
+    private long clientId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id")
+    private Advisor advisor;
+
+    @OneToOne(mappedBy = "client")
+    private Portfolio portfolio;
+
+    protected Client() {}
+
+    public Client(String firstName, String lastName,
+                  String email, String phone,
+                  Advisor advisor, Portfolio portfolio) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.advisor = advisor;
+        this.portfolio = portfolio;
+    }
+
+    public Long getClientId() { return clientId; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public Advisor getAdvisor() { return advisor; }
+    public void setAdvisor(Advisor advisor) { this.advisor = advisor; }
+
+    public Portfolio getPortfolio() { return portfolio; }
+    public void setPortfolio(Portfolio portfolio) { this.portfolio = portfolio; }
+}
